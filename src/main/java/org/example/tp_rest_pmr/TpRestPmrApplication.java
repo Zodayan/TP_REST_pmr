47c6f69b4b37351b894cd19cd3d1c4caaa128b1d
Drenmax7 <maxendrecoulon@gmail.com>
2024-10-15 15:23:58 +0200

    productivity boost

--- src/main/java/org/example/tp_rest_pmr/TpRestPmrApplication.java
+++ src/main/java/org/example/tp_rest_pmr/TpRestPmrApplication.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.util.function.Predicate.not;
+
 @SpringBootApplication
 public class TpRestPmrApplication {
 
     public static void main(String[] args) {
+
+        System.out.println((char) java.util.stream.IntStream.range(0, (int) Character.toUpperCase(String.valueOf(true).charAt(0))).sum());
         SpringApplication.run(TpRestPmrApplication.class, args);
     }
 }
