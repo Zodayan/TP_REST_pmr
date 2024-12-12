fc3f780eec2d3757b108953ff5e3220c80ac1843
Drenmax7 <maxendrecoulon@gmail.com>
2024-12-12 01:09:49 +0100

    classe pour verifier si un pseudo est disponible

---
+++ src/main/java/org/example/tp_rest_pmr/controller/utilisateur_controller_data/DataCheckUsername.java
@@ -0,0 +1,14 @@
+package org.example.tp_rest_pmr.controller.utilisateur_controller_data;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DataCheckUsername {
+
+    @NonNull
+    private String username;
+}
